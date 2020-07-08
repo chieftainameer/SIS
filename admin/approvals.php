a9fca64624bac7e1a15a29b3fea4fb10ff3000bd
chieftain <chieftainameer96@gmail.com>
2020-07-08 23:24:03 +0500

    improved approval section with ajax

--- admin/approvals.php
+++ admin/approvals.php
@@ -7,6 +7,9 @@ $s_rk_c = $con->query($s_rk_q);
 $p_rk_q = "SELECT * FROM parents WHERE status=0 LIMIT 0,7";
 $p_rk_c = $con->query($p_rk_q);
 
+$t_rk_q = "SELECT * FROM teacher WHERE status=0 LIMIT 0,7";
+$t_rk_c = $con->query($t_rk_q);
+
 $limit = 7;
 
 $s_q = "SELECT COUNT(*) AS total FROM students WHERE status=0";
@@ -20,6 +23,12 @@ $p_r = $con->query($p_q);
 $p_res = $p_r->fetch_assoc();
 $p_total_rec = $p_res['total'];
 $p_pages = ceil($p_total_rec/$limit);
+
+$t_q = "SELECT COUNT(*) AS total FROM teacher WHERE status=0";
+$t_r = $con->query($t_q);
+$t_res = $t_r->fetch_assoc();
+$t_total_rec = $t_res['total'];
+$t_pages = ceil($t_total_rec/$limit);
 
 
 ?>
@@ -85,7 +94,7 @@ $p_pages = ceil($p_total_rec/$limit);
 		      			 		<td><?= $row['class'] ?></td>
 		      			 		<td><?= $row['roll_num'] ?></td>
 		      			 		<td><?= $row['contact_num'] ?></td>
-		      			 		<td><button class="btn btn-primary" onclick='approve("<?php echo $row['roll_num'] ?>","<?php echo $row['class'] ?>")'>Approve</button></td>
+		      			 		<td><button class="btn btn-primary" onclick='approve("<?php echo $row['roll_num'] ?>","<?php echo $row['class'] ?>","students")'>Approve</button></td>
 		      			 	</tr>
 		      			<?php endwhile; ?>
 		      			<?php else: ?>
@@ -139,7 +148,7 @@ $p_pages = ceil($p_total_rec/$limit);
 		      			 		<td><?= $p_row['class'] ?></td>
 		      			 		<td><?= $p_row['student_roll_num'] ?></td>
 		      			 		<td><?= $p_row['contact_num'] ?></td>
-		      			 		<td><button class="btn btn-primary" onclick='approve("<?php echo $p_row['student_roll_num'] ?>","<?php echo $p_row['class'] ?>")'>Approve</button></td>
+		      			 		<td><button class="btn btn-primary" onclick='approve("<?php echo $p_row['student_roll_num'] ?>","<?php echo $p_row['class'] ?>","parents")'>Approve</button></td>
 		      			 	</tr>
 		      			<?php endwhile; ?>
 		      			<?php else: ?>
@@ -154,7 +163,7 @@ $p_pages = ceil($p_total_rec/$limit);
 		      			<td colspan="7">
 			      			<ul class="pagination">
 			      				<?php for ($i=0; $i < $p_pages ; $i++): ?>
-			      					<li class="page-item"><a  class="page-link text-primary" onclick="paginate('parent-table',<?php echo $i ?>,'parents')"><?= $i+1 ?></a></li>
+			      					<li class="page-item"><a  class="page-link text-primary" onclick="paginate_parent('parent-table',<?php echo $i ?>,'parents')"><?= $i+1 ?></a></li>
 			      				<?php endfor; ?>
 			      			</ul>
 		      		    </td>
@@ -167,8 +176,53 @@ $p_pages = ceil($p_total_rec/$limit);
 
 	    <!-- Teachers Table -->
 	    <div id="teacher" class="container tab-pane fade"><br>
-	      <h3>Menu 2</h3>
-	      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+	       <h3>Parents</h3>
+	      <p>these parent requests needs approval by the admin.<p>
+	      	<table id="teacher-table" class="table table-hover" style="max-height:50px;">
+	      		<thead>
+	      			<tr>
+	      				<th>Name</th>
+	      				<th>Email</th>
+	      				<th>Class</th>
+	      				<th>Qualification</th>
+	      				<th>Contact No</th>
+	      				<th></th>
+	      			</tr>
+	      		</thead>
+	      		<tbody>
+	      			<?php if($t_rk_c->num_rows > 0): ?>
+		      			<?php while($t_row = $t_rk_c->fetch_assoc()): 
+ 							// print_r($s_rk_c->fetch_assoc());
+ 							//print_r($row);
+		      				?>
+		      			 	<tr>
+		      			 		<td><?= $t_row['name'] ?></td>
+		      			 		<td><?= $t_row['email'] ?></td>
+		      			 		<td><?= $t_row['class'] ?></td>
+		      			 		<td><?= $t_row['qualification'] ?></td>
+		      			 		<td><?= $t_row['contact_num'] ?></td>
+		      			 		<td><button class="btn btn-primary" onclick='approve("<?php echo $t_row['email'] ?>","<?php echo $t_row['class'] ?>","teachers")'>Approve</button></td>
+		      			 	</tr>
+		      			<?php endwhile; ?>
+		      			<?php else: ?>
+		      				<tr>
+		      					<td colspan="7">No Data to show</td>
+		      				</tr>
+		      			<?php endif; ?>
+	      			 
+	      		</tbody>
+	      		<tfoot>
+	      			<tr>
+		      			<td colspan="7">
+			      			<ul class="pagination">
+			      				<?php for ($i=0; $i < $t_pages ; $i++): ?>
+			      					<li class="page-item"><a  class="page-link text-primary" onclick="paginate_teacher('teacher-table',<?php echo $i ?>,'teacher')"><?= $i+1 ?></a></li>
+			      				<?php endfor; ?>
+			      			</ul>
+		      		    </td>
+	      		    </tr>
+	      		</tfoot>
+	      	</table>
 	    </div>
 	  </div>
    </div>
@@ -187,13 +241,13 @@ $p_pages = ceil($p_total_rec/$limit);
     function paginate(tbl,page,dbTable){
 
     	$.ajax({
-    		url:'paginate.php',
+    		url:'paginate_parent.php',
     		type:'post',
     		data:{'page':page,'table':dbTable},
     		dataType:'json',
     		success:function(data,status){
     			console.log(data);
-    			$('#student-table tbody').empty();
+    			$(`#${tbl} tbody`).empty();
     			$.each(data,function(d,v){
     				$(`#${tbl} tbody`).append(`<tr>
     					<td>${v.name}</td>
@@ -209,9 +263,59 @@ $p_pages = ceil($p_total_rec/$limit);
     	});
     }
 
+    function paginate_parent(tbl,page,dbTable){
 
-    function approve(r,c){
-    	alert("working " + r + " " +c);
+    	$.ajax({
+    		url:'paginate.php',
+    		type:'post',
+    		data:{'page':page,'table':dbTable},
+    		dataType:'json',
+    		success:function(data,status){
+    			console.log(data);
+    			$(`#${tbl} tbody`).empty();
+    			$.each(data,function(d,v){
+    				$(`#${tbl} tbody`).append(`<tr>
+    					<td>${v.name}</td>
+    					<td>${v.email}</td>
+    					<td>${v.class}</td>
+    					<td>${v.student_roll_num}</td>
+    					<td>${v.contact_num}</td>
+    					<td><button class="bt btn-primary" onclick='approve("${v.student_roll_num}","${v.class}")'>Approve</button></td>
+    					</tr>`);
+    			});
+    			
+    		},
+    	});
+    }
+
+    function paginate_teacher(tbl,page,dbTable){
+
+    	$.ajax({
+    		url:'paginate_teacher.php',
+    		type:'post',
+    		data:{'page':page,'table':dbTable},
+    		dataType:'json',
+    		success:function(data,status){
+    			console.log(data);
+    			$(`#${tbl} tbody`).empty();
+    			$.each(data,function(d,v){
+    				$(`#${tbl} tbody`).append(`<tr>
+    					<td>${v.name}</td>
+    					<td>${v.email}</td>
+    					<td>${v.class}</td>
+    					<td>${v.qualification}</td>
+    					<td>${v.contact_num}</td>
+    					<td><button class="bt btn-primary" onclick='approve("${v.email}","${v.class}")'>Approve</button></td>
+    					</tr>`);
+    			});
+    			
+    		},
+    	});
+    }
+
+
+    function approve(r,c,section){
+    	alert("working " + r + " " +c + " "+section);
     }
 
 
